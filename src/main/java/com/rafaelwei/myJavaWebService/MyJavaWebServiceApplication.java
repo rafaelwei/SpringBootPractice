d49e69e12aa4d1269e7853594857346592e644a8
rafaelwei <rafaelwei07@gmail.com>
2020-09-14 15:58:17 -0300

    Project created

---
+++ src/main/java/com/rafaelwei/myJavaWebService/MyJavaWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelwei.myJavaWebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJavaWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyJavaWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafaelwei/myJavaWebService/MyJavaWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelwei.myJavaWebService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyJavaWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
